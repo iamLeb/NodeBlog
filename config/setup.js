f753341e3629a8d9db1a36603249f7cc439feceb
Caleb Joseph <calebbjoseph@gmail.com>
2023-11-18 09:39:59 -0600

    added model and intigrated the passport pluggin

--- config/setup.js
+++ config/setup.js
@@ -1,6 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 require('dotenv').config();
+const User = require('../models/User');
+const {redirect} = require("../controllers/PageController");
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => done(null, user));
+});
 
 passport.use(
     new GoogleStrategy({
@@ -10,6 +21,23 @@ passport.use(
         scope: ['profile']
     }, (accessToken, refreshToken, profile, done) => {
         // catch call back from Google
-        console.log('Code here -- testing ')
+        // console.log(profile.photos[0].value);
+
+        //check if user already exists
+        User.findOne({googleid: profile.id})
+            .then(existUser => {
+                if (existUser) {
+                    // do not add new user
+                    done(null, existUser);
+                } else {
+                    new User({
+                        googleId: profile.id,
+                        name: profile.displayName,
+                        photo: profile.photos[0].value
+                    }).save().then(newUser => {
+                        done(null, newUser);
+                    });
+                }
+        });
     })
 );
